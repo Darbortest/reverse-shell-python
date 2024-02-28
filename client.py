50d865a51a0c9bb96e535d6b2bf887e28d43e809
Darbortest <159553871+Darbortest@users.noreply.github.com>
2024-02-28 11:43:56 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,32 +1,18 @@
 import requests
 
-
-
 ipaddr = '10.9.3.102'  # Replace with the actual server IP address
-
 port = 4201               # Replace with the actual server port
 
-
-
 def send_command(command):
-
     url = f'http://{ipaddr}:{port}/commander/run?command={command}'
-
     response = requests.get(url)
-
     print(response.text)
 
 
-
 while True:
-
     user_input = input("Enter command (type 'exit' to quit): ")
-
     if user_input.lower() == 'exit':
-
         break
-
-
 
     send_command(user_input)
 
